0418e36f16b5fc9007f33c9649b459c4a5ae115d
pvogenthaler <paige.vogenthaler@gmail.com>
2017-01-03 16:53:20 -0800

    (feat) Implement Facebook login button and auth for iOS (#20)
    * Add FacebookSDK Frameworks, Uninstall react-native-auth
    
    * Implement facebook login button

--- ios/Sonder/AppDelegate.m
+++ ios/Sonder/AppDelegate.m
@@ -12,7 +12,14 @@
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
 @implementation AppDelegate
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -31,7 +38,19 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                         didFinishLaunchingWithOptions:launchOptions];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                         openURL:url
+                                               sourceApplication:sourceApplication
+                                                      annotation:annotation];
 }
 
 @end
