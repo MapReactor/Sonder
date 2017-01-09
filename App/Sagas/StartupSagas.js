9d3e81a47359d6415eeaa4f222a4aa6a8b4a228b
George A. Michel <sarumonakuba@gmail.com>
2017-01-09 03:49:02 -0800

    Move compass-related code into singleton, remove tron comments

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -10,28 +10,28 @@ export const selectTemperature = (state) => state.temperature.temperature
 export function * startup (action) {
   if (__DEV__ && console.tron) {
     // straight-up string logging
-    console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
+    // console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
 
     // logging an object for better clarity
-    console.tron.log({
-      message: 'pass objects for better logging',
-      someGeneratorFunction: selectTemperature
-    })
+    // console.tron.log({
+    //   message: 'pass objects for better logging',
+    //   someGeneratorFunction: selectTemperature
+    // })
 
     // fully customized!
     const subObject = { a: 1, b: [1, 2, 3], c: true }
     subObject.circularDependency = subObject // osnap!
-    console.tron.display({
-      name: '🔥 IGNITE 🔥',
-      preview: 'You should totally expand this',
-      value: {
-        '💃': 'Welcome to the future!',
-        subObject,
-        someInlineFunction: () => true,
-        someGeneratorFunction: startup,
-        someNormalFunction: selectTemperature
-      }
-    })
+    // console.tron.display({
+    //   name: '🔥 IGNITE 🔥',
+    //   preview: 'You should totally expand this',
+    //   value: {
+    //     '💃': 'Welcome to the future!',
+    //     subObject,
+    //     someInlineFunction: () => true,
+    //     someGeneratorFunction: startup,
+    //     someNormalFunction: selectTemperature
+    //   }
+    // })
   }
   const temp = yield select(selectTemperature)
   // only fetch new temps when we don't have one yet
