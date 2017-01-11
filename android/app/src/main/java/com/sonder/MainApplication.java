f7f28f6e6c092a78100db7d84cf7510873c6e1b6
George A. Michel <sarumonakuba@gmail.com>
2017-01-11 12:35:25 -0800

    (feat) Add Facebok login support to Android (#42)

--- android/app/src/main/java/com/sonder/MainApplication.java
+++ android/app/src/main/java/com/sonder/MainApplication.java
@@ -21,7 +21,18 @@ import java.util.List;
 
 import com.joshblour.reactnativeheading.ReactNativeHeadingPackage;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
+
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -39,7 +50,8 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactNativeI18n(),
             new RNDeviceInfo(),
             new ReactNativeConfigPackage(),
-            new ReactNativeHeadingPackage()
+            new ReactNativeHeadingPackage(),
+            new FBSDKPackage(mCallbackManager)
       );
     }
   };
@@ -52,6 +64,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
