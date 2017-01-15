062e9ba4c1f7b09cfa3f3046d078131ce4ee4a52
Paige <paige.vogenthaler@gmail.com>
2017-01-14 17:03:06 -0800

    friends locations redux cleanup

--- App/Redux/FriendsLocationsRedux.js
+++ App/Redux/FriendsLocationsRedux.js
@@ -7,8 +7,6 @@ import Immutable from 'seamless-immutable'
 
 const { Types, Creators } = createActions({
   friendsLocationsUpdate: ['friendsLocations'],
-  // friendsLocationsSuccess: ['friendsLocations'],
-  // friendsLocationsFailure: null,
 })
 
 export const FriendsLocationsTypes = Types
@@ -20,8 +18,7 @@ export const INITIAL_STATE = Immutable({
   friendsLocations: {
     '123': { 'longitude': '1', 'latitude': '1', },
     '456': { 'longitude': '1', 'latitude': '2' }
-  },  // latitude, longitude, bearing, picture
-  // error: null,
+  },
 })
 
 /* ------------- Reducers ------------- */
@@ -29,34 +26,18 @@ export const INITIAL_STATE = Immutable({
 export const update = (state: Object, action: Object) => {
   const { friendsLocations } = action
   if (friendsLocations) {
-
     let newLocation = {}
     newLocation[friendsLocations.id] = {
       'latitude': friendsLocations.latitude,
       'longitude': friendsLocations.longitude
     }
-    console.log('newLocation: ', newLocation)
-
     return Object.assign({}, state, Object.assign({}, state.friendsLocations, newLocation))
   }
   return state
 }
 
-// we've successfully got friends' locations
-// export const success = (state: Object, { friendLocation }: Object) => {
-  // update particular friend location
-  // console.log(friendsLocations);
-// }
-
-// we've had a problem getting friends' locations
-// export const failure = (state: Object, { error }: Object) => {
-//   state.merge({ error })
-// }
-
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.FRIENDS_LOCATIONS_UPDATE]: update,
-  // [Types.FRIENDS_LOCATIONS_SUCCESS]: success,
-  // [Types.FRIENDS_LOCATIONS_FAILURE]: failure,
 })
