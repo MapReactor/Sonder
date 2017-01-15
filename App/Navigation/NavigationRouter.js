f9ed3573f432c9f34b788fbf21d1316cd80e3e53
George A. Michel <sarumonakuba@gmail.com>
2017-01-15 11:43:36 -0800

    (fix) Move fixture to correct location, rename AnsibleView

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -12,7 +12,7 @@ import MapViewExample from '../Containers/MapViewExample'
 import WelcomeView from '../Containers/WelcomeView'
 import StorageTestView from '../Containers/StorageTestView'
 import FriendsView from '../Containers/FriendsView'
-import AnsibleView from '../Containers/AnsibleView'
+import SonderView from '../Containers/SonderView'
 import MapBoxExample from '../Containers/MapBoxExample'
 import MapBoxExampleSf from '../Containers/MapBoxExampleSf'
 
@@ -27,7 +27,7 @@ class NavigationRouter extends Component {
         <Scene key='drawer' component={NavigationDrawer} open={false}>
           <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
             <Scene initial key='welcomeView' hideNavBar={false} component={WelcomeView} />
-            <Scene key='ansibleView' component={AnsibleView} title='Sonder Ansible' />
+            <Scene key='sonderView' component={SonderView} title='Sonder' />
             {/* <Scene key='compassView' hideNavBar={false} component={CompassView} title='Compass (deprecated)' />*/}
             <Scene key='mapBoxExampleView' component={MapBoxExample} title='MapBox Example' />
             <Scene key='mapBoxExampleSfView' component={MapBoxExampleSf} title='MapBox Example SF' />
