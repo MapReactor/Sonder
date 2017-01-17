1b8c4290c33e1779227684e3e204713e109a1962
pvogenthaler <paige.vogenthaler@gmail.com>
2017-01-17 11:32:05 -0800

    Refactor FriendsView to user FriendsHelper (#56)
    * Display markers from redux store coordinates, and update dynamically
    
    * Clean up friendsview
    
    * Refactor FriendsView to user FriendsHelper

---
+++ App/Lib/FriendsHelpers.js
@@ -0,0 +1,22 @@
+
+export default {
+
+  updateFriendsLocations: (nextProps, prevState) => {
+    console.log('in updateFriendsLocations')
+    var arr = [ ...prevState.annotations ]
+    for (let newPoint in nextProps.friendsLocations) {
+      let friendLocation = nextProps.friendsLocations
+      if (friendLocation[newPoint].longitude) {
+        arr.push ({
+            coordinates: [ friendLocation[newPoint].longitude, friendLocation[newPoint].latitude ],
+            type: 'point',
+            title: newPoint,
+            id: newPoint,
+        })
+      }
+    }
+    console.log('arr', arr)
+    return { annotations: arr }
+  }
+
+}
