dda3605ca34bcce905f864c2745689f36f2b487e
pvogenthaler <paige.vogenthaler@gmail.com>
2017-01-19 19:33:13 -0800

    Remove unnecessary files (#69)

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -10,7 +10,6 @@ export default () => {
     location: require('./LocationRedux').reducer,
     user: require('./UserRedux').reducer,
     // search: require('./SearchRedux').reducer,
-    storageTest: require('./StorageTestRedux').reducer,
     friendsLocations: require('./FriendsLocationsRedux').reducer,
   })
 
