9b5ef51b581142f51770c3c85788a9b7e765830f
pvogenthaler <paige.vogenthaler@gmail.com>
2017-01-19 21:24:48 -0800

    Start yelp popup styling (#71)

---
+++ App/Containers/Styles/PopupStyle.js
@@ -0,0 +1,34 @@
+export default {
+  yelpRating: {
+    height: 25,
+    width: 125,
+    right: 0,
+    position: "absolute",
+  },
+  yelpTitle: {
+    fontSize: 18,
+  },
+  yelpTitleContainer: {
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  yelpCategories: {
+    color: "gray",
+  },
+  yelpReviewCount: {
+    color: "gray",
+    fontSize: 10,
+    right: 0,
+    position: "absolute",
+  },
+  yelpCategoriesContainer: {
+    marginTop: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+}
